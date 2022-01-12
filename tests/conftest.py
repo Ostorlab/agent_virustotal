344f53c8a9049898ff0903a5e6904f32b05015cc
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-12 09:22:47 +0100

    Added conftest file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,43 @@
-"""
-    Dummy conftest.py for template_agent.
+"""Pytest fixtures for the virustotal agent."""
+import pytest
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
-"""
+from ostorlab.agent import message
+from ostorlab.agent import definitions as agent_definitions
+from ostorlab.runtimes import definitions as runtime_definitions
 
-# import pytest
+from src import agent as virustotal_agent
+
+
+@pytest.fixture(name='message')
+def create_scan_message():
+    """Creates a dummy message of type v3.asset.file to be used by the agent for testing purposes.
+    The files used is the EICAR Anti-Virus Test File.
+    """
+    with open('files/malicious_dummy.com', 'rb') as f:
+        file_content = f.read()
+        selector = 'v3.asset.file'
+        msg_data = {'content': file_content, 'path': 'some/dummy/path'}
+        return message.Message.from_data(selector, data=msg_data)
+
+@pytest.fixture(name='virustotal_agent')
+def create_virustotal_agent():
+    """Instantiate a virustotal agent."""
+    definition = agent_definitions.AgentDefinition(
+        name='agent_virustotal',
+        in_selectors = ['v3.healthcheck.ping', 'v3.asset.file'],
+        out_selectors=['v3.report.event.vulnerability']
+    )
+
+    settings = runtime_definitions.AgentSettings(
+        key='agent_virustotal_key',
+        bus_url='NA',
+        bus_exchange_topic='NA',
+    )
+
+    agent = virustotal_agent.VirusTotalAgent(
+        definition,
+        settings,
+        api_key='some_dumy_api_key'
+    )
+
+    return agent
