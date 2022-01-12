97e7c7831613c7333175c7f8edc9252d19ae4c6b
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-12 09:22:03 +0100

    Module for interacting with virustotal api.

---
+++ src/virustotal.py
@@ -0,0 +1,47 @@
+"""Module responsible for interacting with virustotal public api."""
+from typing import Dict
+import hashlib
+
+from virus_total_apis import PublicApi as VirusTotalPublicApi
+from ostorlab.agent import message as agent_message
+
+class Error(BaseException):
+    """Custom Error."""
+
+
+class VirusTotalApiError(Error):
+    """VirtualTotalApiError."""
+
+def scan_file(message: agent_message.Message, api_key: str) -> Dict:
+    """Method responsible for scanning a file through the virustotal public api.
+    Args:
+        message: Message containing the file to scan.
+        api_key : Key for the virustotal api.
+    Returns:
+        response: The response of the virustotal public api.
+    """
+    file = message.data['content']
+    file_md5_hash = hashlib.md5(file)
+    hash_hexa = file_md5_hash.hexdigest()
+    virustotal_client = VirusTotalPublicApi(api_key)
+    response = virustotal_client.get_file_report(hash_hexa)
+    return response
+
+
+def get_scans(response: Dict) -> Dict:
+    """Method that returns the scans from the virustotal public api response.
+    Args:
+        response: Dictionary of the api response.
+    Returns:
+        scans: Dictionary of the scans.
+    Raises:
+        VirusTotalApiError: In case the API request encountered problems.
+    """
+    # if 'results' not in response:
+    #     raise VirusTotalApiError
+    if response['response_code'] == 0:  #better than the lines before ?
+        raise VirusTotalApiError
+    elif response['results']['response_code'] == 1:
+        return response['results']['scans']
+    else:
+        return None
