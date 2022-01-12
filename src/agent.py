cac9bf23b0b508c9b1023f81aa576f9ba93e4c1e
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-01-12 14:17:52 +0100

    Used process_scans module.

--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,11 @@
 """VirusTotal agent implementation : Agent responsible for scanning a file through the Virus Total DB."""
 import logging
-from typing import Dict
 
 from ostorlab.agent import agent
 from ostorlab.agent import message as msg
-from . import markdown
+
 from . import virustotal
+from . import process_scans
 
 
 logger = logging.getLogger(__name__)
@@ -24,7 +24,11 @@ class VirusTotalAgent(agent.Agent):
         self.api_key = api_key
 
     def process(self, message: msg.Message) -> None:
-        """Overriden message processing method."""
+        """Process message of type v3.asset.file;
+        scan the file content throught the Virus Total public API,
+        assign a risk rating, a technical report
+        and emits a message of type v3.report.vulnerability .
+        """
         response = virustotal.scan_file(message, self.api_key)
 
         try:
@@ -34,8 +38,8 @@ class VirusTotalAgent(agent.Agent):
             raise e
 
         if scans:
-            risk_rating = self._get_risk_rating(scans)
-            technical_detail = self._get_technical_details(scans)
+            risk_rating = process_scans.get_risk_rating(scans)
+            technical_detail = process_scans.get_technical_details(scans)
             title = 'VirusTotal report'
             self.emit(
                 'v3.report.event.vulnerability',
@@ -45,25 +49,3 @@ class VirusTotalAgent(agent.Agent):
                     'risk_rating': risk_rating
                 }
             )
-
-    def _get_risk_rating(self, scans:Dict) -> str:
-        """Method responsible for assigning a risk level to the scanned file.
-        Returns:
-            'HIGH' if at least one anti-virus detected the file as a virus, else Secure.
-        """
-        for scanner_result in scans.values():
-            if scanner_result['detected']:
-                return 'HIGH'
-        return 'SECURE'
-
-    def _get_technical_details(self, scans:Dict) -> str:
-        """Method that returns a markdwon table of the Virus Total scan.
-        Each row presents an antivirus with corresponding scan result : Malicious/Safe.
-        Args:
-            scans : Dictionary of the scans.
-        Returns:
-            technical_detail : Markdown table of the scans results.
-        """
-        scans = markdown.prepare_data_for_markdown_formatting(scans)
-        technical_detail = markdown.table_markdown(scans)
-        return technical_detail
