60e1c284a25096dc2ea18cd9cc39b9d47aec001a
asasas333_3 <am.mesbahi@gmail.com>
2022-01-26 15:44:32 +0100

    fix path imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from ostorlab.agent import definitions as agent_definitions
 from ostorlab.runtimes import definitions as runtime_definitions
 from ostorlab.utils import defintions as args_definitions
 
-import agent as virustotal_agent
+from agent import agent as virustotal_agent
 
 
 @pytest.fixture(name='message')
